aee6a6582595b440d179428a894f0f8f36ce84c7
rikish1 <avzabolotskiy@edu.hse.ru>
2024-02-07 10:49:46 +0500

    All figures and functions

--- MDIPaint/MainWindow.xaml.cs
+++ MDIPaint/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Ink;
@@ -20,13 +21,34 @@ namespace MDIPaint
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Цвет пера
+        /// </summary>
         private static Color PenColor { get; set; }
+        /// <summary>
+        /// Размер пера
+        /// </summary>
         private static double PenSize { get; set; }
+        /// <summary>
+        /// Коллекция всех текущих холстов
+        /// </summary>
         private static ObservableCollection<InkCanvas> InkCanvases = new ObservableCollection<InkCanvas>();
+        /// <summary>
+        /// Текущая линия
+        /// </summary>
         private Line currentLine;
+        /// <summary>
+        /// Текущий эллипс
+        /// </summary>
         private Ellipse currentEllipse;
+        /// <summary>
+        /// Текущая точка
+        /// </summary>
         private Point currentPoint;
 
+        /// <summary>
+        /// Иничиализация при запуске окна
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
@@ -50,11 +72,17 @@ namespace MDIPaint
             EightPxSizeButton.IsEnabled = false;
             ZoomInButton.IsEnabled = false;
             ZoomOutButton.IsEnabled = false;
+            StarSettings.Visibility = Visibility.Hidden;
             PenColor = Color.FromRgb(0, 0, 0);
             PenSize = 3.0;
             InkCanvases.CollectionChanged += InkCanvases_CollectionChanged;
         }
 
+        /// <summary>
+        /// Событие при изменении коллекции
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e">Действие над коллекцией</param>
         private void InkCanvases_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
             // Событие сработает при любых изменениях в коллекции
@@ -114,22 +142,45 @@ namespace MDIPaint
                         EightPxSizeButton.IsEnabled = false;
                         ZoomInButton.IsEnabled = false;
                         ZoomOutButton.IsEnabled = false;
+                        StarSettings.Visibility = Visibility.Hidden;
                     }
                     break;
             }
         }
-        
+        /// <summary>
+        /// Событие при изменении элемента внутри коллекции
+        /// </summary>
+        /// <param name="sender">Изменяемый холст</param>
+        /// <param name="e"></param>
+        private void InkCanvas_StrokeCollected(object sender, InkCanvasStrokeCollectedEventArgs e)
+        {
+            ((InkCanvas)sender).Tag = false; // Устанавливаем флаг изменений
+        }
+
+        /// <summary>
+        /// Событие при нажатии на кнопку Выход
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
             MainForm.Close();
         }
-
+        /// <summary>
+        /// Событие при нажатии на кнопку О программе
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AboutButton_Click(object sender, RoutedEventArgs e)
         {
             var AboutForm = new AboutForm();
             AboutForm.ShowDialog();
         }
-
+        /// <summary>
+        /// Событие при нажатии на кнопку Создать
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CreateButton_Click(object sender, RoutedEventArgs e)
         {
             var inkCanvas = new InkCanvas
@@ -140,11 +191,13 @@ namespace MDIPaint
                 {
                     Color = PenColor,
                     Width = PenSize,
-                    Height = PenSize
+                    Height = PenSize,
                 }
             }; ;
             ScaleTransform scale = new ScaleTransform();
             inkCanvas.LayoutTransform = scale;
+            inkCanvas.StrokeCollected += InkCanvas_StrokeCollected;
+            inkCanvas.Tag = false;
             InkCanvases.Add(inkCanvas);
 
             var NewMDIChild = new WPF.MDI.MdiChild()
@@ -153,8 +206,37 @@ namespace MDIPaint
                 Cursor = Cursors.Pen,
                 Content = inkCanvas
             };
-            NewMDIChild.Closed += (s, args) =>
+            NewMDIChild.Closing += (s, args) =>
             {
+                if ((bool)inkCanvas.Tag == false)
+                {
+                    MessageBoxResult result = MessageBox.Show("Изменения не сохранены. Хотите сохранить изменения перед закрытием?", "Предупреждение", MessageBoxButton.YesNo);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        if (NewMDIChild.Tag == null)
+                        {
+                            var saveFileDialog = new SaveFileDialog
+                            {
+                                Filter = "Bitmap (*.bmp)|*.bmp|JPEG (*.jpg)|*.jpg|PNG (*.png)|*.png",
+                                DefaultExt = "png"
+                            };
+
+                            if (saveFileDialog.ShowDialog() == true)
+                            {
+                                SaveInkCanvas(inkCanvas, saveFileDialog.FileName);
+                                // Сохраняем путь к файлу в свойствах MdiChild
+                                NewMDIChild.Tag = saveFileDialog.FileName;
+                                NewMDIChild.Tag = true;
+                            }
+                        }
+                        else
+                        {
+                            // Если это не первое сохранение, используем сохраненный путь к файлу
+                            SaveInkCanvas(inkCanvas, (string)NewMDIChild.Tag);
+                            inkCanvas.Tag = true;
+                        }
+                    }
+                }
                 InkCanvases.Remove(inkCanvas);
                 if (InkCanvases.Count == 0)
                 {
@@ -163,7 +245,6 @@ namespace MDIPaint
                     SmallSaveButton.IsEnabled = false;
                 }
             };
-            // Удаляем InkCanvas из списка при закрытии MdiChild
             MdiContainer.Children.Add(NewMDIChild);
             if (InkCanvases.Count == 0)
             {
@@ -178,7 +259,6 @@ namespace MDIPaint
                 SmallSaveButton.IsEnabled = true;
             }
         }
-
         #region Изменение цвета пера
         private void RedColorButton_Checked(object sender, RoutedEventArgs e)
         {
@@ -306,12 +386,14 @@ namespace MDIPaint
                     SaveInkCanvas(activeInkCanvas, saveFileDialog.FileName);
                     // Сохраняем путь к файлу в свойствах MdiChild
                     activeMdiChild.Tag = saveFileDialog.FileName;
+                    activeInkCanvas.Tag = true;
                 }
             }
             else
             {
                 // Если это не первое сохранение, используем сохраненный путь к файлу
                 SaveInkCanvas(activeInkCanvas, (string)activeMdiChild.Tag);
+                activeInkCanvas.Tag = true;
             }
 
         }
@@ -385,6 +467,7 @@ namespace MDIPaint
 
         private void PenButton_Checked(object sender, RoutedEventArgs e)
         {
+            StarSettings.Visibility = Visibility.Hidden;
             foreach (var canvas in InkCanvases)
             {
                 canvas.EditingMode = InkCanvasEditingMode.Ink;
@@ -394,11 +477,13 @@ namespace MDIPaint
                 canvas.MouseDown -= LineDrawing_MouseDown;
                 canvas.MouseMove -= LineDrawing_MouseMove;
                 canvas.MouseUp -= LineDrawing_MouseUp;
+                canvas.MouseDown -= StarDrawing_MouseDown;
             }
         }
 
         private void EraserButton_Checked(object sender, RoutedEventArgs e)
         {
+            StarSettings.Visibility = Visibility.Hidden;
             foreach (var canvas in InkCanvases)
             {
                 canvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
@@ -408,17 +493,20 @@ namespace MDIPaint
                 canvas.MouseDown -= LineDrawing_MouseDown;
                 canvas.MouseMove -= LineDrawing_MouseMove;
                 canvas.MouseUp -= LineDrawing_MouseUp;
+                canvas.MouseDown -= StarDrawing_MouseDown;
             }
         }
 
         private void LineButton_Checked(object sender, RoutedEventArgs e)
         {
+            StarSettings.Visibility = Visibility.Hidden;
             foreach (var canvas in InkCanvases)
             {
                 // Удаляем обработчики событий для других инструментов
                 canvas.MouseDown -= EllipseDrawing_MouseDown;
                 canvas.MouseMove -= EllipseDrawing_MouseMove;
                 canvas.MouseUp -= EllipseDrawing_MouseUp;
+                canvas.MouseDown -= StarDrawing_MouseDown;
 
                 canvas.EditingMode = InkCanvasEditingMode.None;
                 canvas.MouseDown += LineDrawing_MouseDown;
@@ -428,17 +516,35 @@ namespace MDIPaint
         }
         private void EllipseButton_Checked(object sender, RoutedEventArgs e)
         {
+            StarSettings.Visibility = Visibility.Hidden;
             foreach (var canvas in InkCanvases)
             {
                 // Удаляем обработчики событий для других инструментов
                 canvas.MouseDown -= LineDrawing_MouseDown;
                 canvas.MouseMove -= LineDrawing_MouseMove;
                 canvas.MouseUp -= LineDrawing_MouseUp;
+                canvas.MouseDown -= StarDrawing_MouseDown;
 
                 canvas.EditingMode = InkCanvasEditingMode.None;
                 canvas.MouseDown += EllipseDrawing_MouseDown;
                 canvas.MouseMove += EllipseDrawing_MouseMove;
-                canvas.MouseUp += EllipseDrawing_MouseUp;
+                canvas.MouseUp += EllipseDrawing_MouseUp; 
+            }
+        }
+        private void StarButton_Checked(object sender, RoutedEventArgs e)
+        {
+            StarSettings.Visibility = Visibility.Visible;
+            foreach(var canvas in InkCanvases)
+            {
+                canvas.MouseDown -= LineDrawing_MouseDown;
+                canvas.MouseMove -= LineDrawing_MouseMove;
+                canvas.MouseUp -= LineDrawing_MouseUp;
+                canvas.MouseDown -= EllipseDrawing_MouseDown;
+                canvas.MouseMove -= EllipseDrawing_MouseMove;
+                canvas.MouseUp -= EllipseDrawing_MouseUp;
+
+                canvas.EditingMode = InkCanvasEditingMode.None;
+                canvas.MouseDown += StarDrawing_MouseDown;
             }
         }
 
@@ -477,6 +583,7 @@ namespace MDIPaint
         }
         #endregion
 
+        #region Эллипс
         private void EllipseDrawing_MouseDown(object sender, MouseButtonEventArgs e)
         {
             var inkCanvas = sender as InkCanvas;
@@ -529,6 +636,121 @@ namespace MDIPaint
             currentEllipse = null;
         }
 
+        #endregion
+
+        #region Звезда
+        /// <summary>
+        /// Событие клика при рисовании звезды
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StarDrawing_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var inkCanvas = sender as InkCanvas;
+            if (inkCanvas == null) return;
+
+            // Получаем координаты клика мыши
+            var mousePosition = e.GetPosition(inkCanvas);
+
+            // Получаем параметры звезды из текстовых полей
+            int numPoints = int.Parse(StarPointCount.Text);
+            double innerRadiusRatio = double.Parse(RadiusQuotient.Text);
+
+            // Создаем звезду
+            var star = CreateStar(mousePosition, numPoints, innerRadiusRatio);
+
+            // Добавляем звезду на холст
+            //var starStroke = new Stroke(new StylusPointCollection(((Polygon)star).Points.Select(p => new StylusPoint(p.X, p.Y))));
+            inkCanvas.Children.Add(star);
+        }
+        /// <summary>
+        /// Логика рисования звезды
+        /// </summary>
+        /// <param name="center">Точка нажатия</param>
+        /// <param name="numPoints">Число лучей</param>
+        /// <param name="innerRadiusRatio">Отношение радиусов</param>
+        /// <returns></returns>
+        private Shape CreateStar(Point center, int numPoints, double innerRadiusRatio)
+        {
+            var points = new PointCollection();
+            double angleStep = Math.PI / numPoints;
+            double outerRadius = 50; // Вы можете изменить это значение в соответствии с вашими потребностями
+            double innerRadius = outerRadius * innerRadiusRatio;
+
+            for (int i = 0; i < 2 * numPoints; i++)
+            {
+                double radius = i % 2 == 0 ? outerRadius : innerRadius;
+                double angle = i * angleStep;
+                points.Add(new Point(center.X + radius * Math.Cos(angle), center.Y + radius * Math.Sin(angle)));
+            }
+
+            var star = new Polygon
+            {
+                Points = points,
+                Stroke = new SolidColorBrush(PenColor), // Используйте вашу глобальную переменную PenColor
+                StrokeThickness = PenSize, // Используйте вашу глобальную переменную PenSize
+                Fill = Brushes.Transparent
+            };
+            return star;
+        }
+        /// <summary>
+        /// Запрет на ввод нечисел
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StarPointCount_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, 0))
+            {
+                e.Handled = true;
+            }
+        }
+        /// <summary>
+        /// Запрет на ввод нечисел
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RadiusQuotient_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, 0))
+            {
+                e.Handled = true;
+            }
+        }
+        /// <summary>
+        /// Автоустановка 0 в поле
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StarPointCount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "0";
+            }
+        }
+        /// <summary>
+        /// Автоустановка 0 в поле
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RadiusQuotient_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "0";
+            }
+        }
+        #endregion
+
+        #region Масштаб
+        /// <summary>
+        /// Увеличение масштаба активного холста
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ZoomInButton_Click(object sender, RoutedEventArgs e)
         {
             var activeMdiChild = MdiContainer.ActiveMdiChild as MdiChild;
@@ -538,7 +760,11 @@ namespace MDIPaint
             scale.ScaleX *= 2;
             scale.ScaleY *= 2;
         }
-
+        /// <summary>
+        /// Уменьшение масштаба активного холста
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ZoomOutButton_Click(object sender, RoutedEventArgs e)
         {
             var activeMdiChild = MdiContainer.ActiveMdiChild as MdiChild;
@@ -548,5 +774,6 @@ namespace MDIPaint
             scale.ScaleX /= 2;
             scale.ScaleY /= 2;
         }
+        #endregion
     }
 }
